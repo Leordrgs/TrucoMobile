e7782e5e00be45c1d795a9ae3f1df558e9521a6d
ruan-cardozo <ruanvcardozo@gmail.com>
2024-04-08 23:17:21 -0300

    adding tests for cards class, and refactoring some methods

--- truco_mobile/lib/src/model/card_model.dart
+++ truco_mobile/lib/src/model/card_model.dart
@@ -1,32 +1,23 @@
 class Card {
-  final String rank;
-  final String suit;
-  final int value;
+  String rank;
+  String suit;
+  int value;
 
   Card({required this.rank, required this.suit, required this.value});
 
-  
-
   int compareValue(Card other) {
-    if (value > other.value) {
-      return 1;
-    } else if (value < other.value) {
-      return -1;
-    } else {
-      // Tratar caso de empate
-      return 0;
-    }
+    return value.compareTo(other.value);
   }
-
 
   @override
   String toString() {
     return '$rank de $suit';
   }
-  Card.fromJson(Map<String, dynamic> json) 
-  : rank = json['rank'],
-    suit = json['suit'],
-    value = json['value'];
+
+  Card.fromJson(Map<String, dynamic> json)
+      : rank = json['rank'],
+        suit = json['suit'],
+        value = json['value'];
 
   Map<String, dynamic> toJson() {
     return {
@@ -34,5 +25,5 @@ class Card {
       'suit': suit,
       'value': value,
     };
-  }  
+  }
 }
