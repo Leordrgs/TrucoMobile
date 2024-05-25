a7dfb91e9649fc56c52bc79db24a3c923dd7c9ea
ruan-cardozo <ruanvcardozo@gmail.com>
2024-05-25 20:53:36 -0300

    adding new pages

---
+++ truco_mobile/lib/src/view/LoginView/login_view.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+import 'package:truco_mobile/src/view/MainPage/main_page.dart';
+import 'package:truco_mobile/src/widget/CustomButton/custom_button.dart';
+import 'package:truco_mobile/src/widget/CustomTextInput/custom_text.dart';
+
+class MyLoginPage extends StatefulWidget {
+  final String title;
+
+  const MyLoginPage({super.key, required this.title});
+
+  @override
+  _MyLoginPage createState() => _MyLoginPage();
+}
+
+class _MyLoginPage extends State<MyLoginPage> {
+  String loginBox = "";
+  final TextEditingController nameController = TextEditingController();
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(style: TextStyle(color: Colors.white), widget.title),
+        backgroundColor: Colors.black,
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back, color: Colors.white),
+          onPressed: () => Navigator.push(context, MaterialPageRoute(builder: (context) => MyMainPagePage())),
+        ),
+      ),
+      body: Container(
+        color: Colors.red,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: <Widget>[
+              Container(
+                padding: EdgeInsets.only(top: 5),
+                width: 276,
+                height: 264,
+                child: Image.network(
+                  'https://gifs.eco.br/wp-content/uploads/2023/11/imagens-de-truco-png-0.png',
+                  fit: BoxFit.cover,
+                ),
+              ),
+              Expanded(
+                child: Scrollbar(
+                  child: ListView(
+                    children: <Widget>[
+                      SizedBox(height: 30),
+                      CustomTextField(
+                          controller: emailController,
+                          hintText: "Digite seu email...",
+                          labelTextFontSize: 36,
+                          hintTextFontSize: 20,
+                          fontSize: 20,
+                          labelText: "Email"),
+                      SizedBox(height: 30),
+                      CustomTextField(
+                          controller: passwordController,
+                          hintText: "Digite sua senha...",
+                          labelTextFontSize: 36,
+                          hintTextFontSize: 20,
+                          fontSize: 20,
+                          obscureText: true,
+                          labelText: "Senha"),
+                      SizedBox(height: 30),
+                      CustomButton(
+                        text: 'Entrar',
+                        onPressed: () => print('Clicou'),
+                        width: 50,
+                        height: 50,
+                        fontSize: 36,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ truco_mobile/lib/src/view/MainPage/main_page_view.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:truco_mobile/src/view/LoginView/login_view.dart';
+import 'package:truco_mobile/src/view/RegisterView/register_view.dart';
+import 'package:truco_mobile/src/widget/CustomButton/custom_button.dart';
+
+class MyMainPagePage extends StatefulWidget {
+  const MyMainPagePage({Key? key}) : super(key: key);
+
+  @override
+  _MyMainPagePageState createState() => _MyMainPagePageState();
+}
+
+class _MyMainPagePageState extends State<MyMainPagePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: Colors.red,
+        width: double.infinity,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisSize: MainAxisSize.max,
+            children: <Widget>[
+              Container(
+                padding: EdgeInsets.only(top: 5),
+                width: 300,
+                height: 286,
+                child: Image.network(
+                  'https://gifs.eco.br/wp-content/uploads/2023/11/imagens-de-truco-png-0.png',
+                  fit: BoxFit.cover,
+                ),
+              ),
+              SizedBox(height: 20), // Espaço entre a imagem e os botões
+              CustomButton(
+                text: 'Entrar',
+                onPressed: () => Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => MyLoginPage(title: 'Login'))),
+                width: 200,
+                height: 50,
+                fontSize: 32,
+              ),
+              SizedBox(height: 10), // Espaço entre os botões
+              CustomButton(
+                text: 'Registrar',
+                onPressed: () => Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) =>
+                            MyRegisterPage(title: 'Registrar'))),
+                width: 200,
+                height: 50,
+                fontSize: 32,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
