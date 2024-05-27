69b2d9a23ec5d1199465cad560940894535c485f
joaoadavid <joao.antoniodavid@hotmail.com>
2024-05-27 13:51:15 -0300

    adding authentication and firebase dependencies

--- truco_mobile/lib/src/view/LoginView/login_view.dart
+++ truco_mobile/lib/src/view/LoginView/login_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:truco_mobile/src/view/HomeView/home_view.dart';
 import 'package:truco_mobile/src/view/MainView/main_page_view.dart';
 import 'package:truco_mobile/src/widget/CustomButton/custom_button.dart';
@@ -7,23 +8,44 @@ import 'package:truco_mobile/src/widget/CustomTextInput/custom_text.dart';
 class MyLoginPage extends StatefulWidget {
   final String title;
 
-  const MyLoginPage({super.key, required this.title});
+  const MyLoginPage({Key? key, required this.title}) : super(key: key);
 
   @override
-  _MyLoginPage createState() => _MyLoginPage();
+  _MyLoginPageState createState() => _MyLoginPageState();
 }
 
-class _MyLoginPage extends State<MyLoginPage> {
-  String loginBox = "";
-  final TextEditingController nameController = TextEditingController();
+class _MyLoginPageState extends State<MyLoginPage> {
   final TextEditingController emailController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  Future<void> _login() async {
+    try {
+      UserCredential userCredential = await _auth.signInWithEmailAndPassword(
+        email: emailController.text,
+        password: passwordController.text,
+      );
+      User? user = userCredential.user;
+      if (user != null) {
+        // Navegar para a tela inicial após o login bem-sucedido
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => MyHomePagePage(title: 'Tela inicial')),
+        );
+      }
+    } on FirebaseAuthException catch (e) {
+      // Mostrar um erro se o login falhar
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(e.message ?? 'Erro ao fazer login')));
+    } catch (e) {
+      print(e);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(style: TextStyle(color: Colors.white), widget.title),
+        title: Text(widget.title, style: TextStyle(color: Colors.white)),
         backgroundColor: Colors.black,
         leading: IconButton(
           icon: Icon(Icons.arrow_back, color: Colors.white),
@@ -51,25 +73,27 @@ class _MyLoginPage extends State<MyLoginPage> {
                     children: <Widget>[
                       SizedBox(height: 30),
                       CustomTextField(
-                          controller: emailController,
-                          hintText: "Digite seu email...",
-                          labelTextFontSize: 36,
-                          hintTextFontSize: 20,
-                          fontSize: 20,
-                          labelText: "Email"),
+                        controller: emailController,
+                        hintText: "Digite seu email...",
+                        labelTextFontSize: 36,
+                        hintTextFontSize: 20,
+                        fontSize: 20,
+                        labelText: "Email",
+                      ),
                       SizedBox(height: 30),
                       CustomTextField(
-                          controller: passwordController,
-                          hintText: "Digite sua senha...",
-                          labelTextFontSize: 36,
-                          hintTextFontSize: 20,
-                          fontSize: 20,
-                          obscureText: true,
-                          labelText: "Senha"),
+                        controller: passwordController,
+                        hintText: "Digite sua senha...",
+                        labelTextFontSize: 36,
+                        hintTextFontSize: 20,
+                        fontSize: 20,
+                        obscureText: true,
+                        labelText: "Senha",
+                      ),
                       SizedBox(height: 30),
                       CustomButton(
                         text: 'Entrar',
-                        onPressed: () => Navigator.push(context, MaterialPageRoute(builder: (context) => MyHomePagePage(title: 'Tela inicial'))),
+                        onPressed: _login,
                         width: 50,
                         height: 50,
                         fontSize: 36,
